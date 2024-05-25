d4b08641400f69636bb6af2bf7998ba11475e9ad
Abh1navgoel459 <abhinavgoel459@berkeley.edu>
2024-05-25 15:43:04 -0400

    temp game page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'game_page.dart';  // Import the GamePage
+import 'game_page_temp.dart';  // Import the Temp GamePage
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'login_page.dart';
-
- 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,25 +11,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  // runApp(const MyApp());
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'ThisIsPoker',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'ThisIsPoker'),
-//     );
-//   }
-// }
 
 class MyApp extends StatelessWidget {
   @override
@@ -55,6 +36,13 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const GamePage()),
+    );
+  }
+
+  void _redirectToTempGamePage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const GamePageTemp()),
     );
   }
 
@@ -91,9 +79,18 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => _redirectToGamePage(context),
-          child: const Text('Play'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _redirectToGamePage(context),
+              child: const Text('Play'),
+            ),
+            ElevatedButton(
+              onPressed: () => _redirectToTempGamePage(context),
+              child: const Text('Temp Play'),
+            ),
+          ],
         ),
       ),
     );

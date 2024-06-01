6fa29b638efe2f98899de59f2bf934bf3c3c3713
rishabh <rishabhgoel0213@gmail.com>
2024-06-01 19:13:18 -0400

    Fixed web version issues and added server scripts to repo

---
+++ server-scripts/create_user.py
@@ -0,0 +1,50 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+import logging
+import time
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("opt/thisispoker/serviceAccountKey.json")  # Update with your own service account key
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Function to create a new user document in Firestore
+def create_user_document(email):
+    user_ref = db.collection('users').document()
+    user_ref.set({
+        'email': email,
+        'match_id': None,
+        'rating': 1000,
+        'searchingForMatch': False
+    })
+
+# Function to handle new user creation
+def handle_user_creation(user):
+    print(f"New user created with email: {user.email}")
+    create_user_document(user.email)
+
+# Listen for new user creation events
+def listen_for_new_users():
+    users = auth.list_users()
+    for user in users.iterate_all():
+        create_user_document(user.email)
+
+# Main function to start listening for new users
+def main():
+    logger.info("Listening for new users...")
+    try:
+        while True:
+            listen_for_new_users()
+            time.sleep(5)  # Check every 5 seconds
+    except Exception as e:
+        logger.error(f"Error in monitor_match: {e}", exc_info=True)
+    listen_for_new_users()
+
+if __name__ == "__main__":
+    main()
---
+++ server-scripts/match_bot.py
@@ -0,0 +1,140 @@
+import os
+import time
+import uuid
+import random
+import logging
+import firebase_admin
+from firebase_admin import credentials, firestore
+from treys import Deck, Evaluator, Card
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Set the environment variable for Firestore (ensure this path is correct)
+cred = credentials.Certificate("/opt/thisispoker/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+# Initialize Firestore client
+db = firestore.client()
+
+# Initialize the evaluator
+evaluator = Evaluator()
+
+def get_hand_type(hand_strength):
+    if hand_strength == 0:
+        return 'Royal Flush'
+    elif hand_strength == 1:
+        return 'Straight Flush'
+    elif hand_strength == 2:
+        return 'Four of a Kind'
+    elif hand_strength == 3:
+        return 'Full House'
+    elif hand_strength == 4:
+        return 'Flush'
+    elif hand_strength == 5:
+        return 'Straight'
+    elif hand_strength == 6:
+        return 'Three of a Kind'
+    elif hand_strength == 7:
+        return 'Two Pair'
+    elif hand_strength == 8:
+        return 'One Pair'
+    elif hand_strength == 9:
+        return 'High Card'
+    return 'Unknown'
+
+
+def evaluate_hands(match_data, match_id):
+    try:
+        player1_cards = match_data['player1']['cards']
+        player2_cards = match_data['player2']['cards']
+        round_names = ['flop', 'turn', 'river']
+        board = []
+        for i in range(match_data['round']):
+            board += match_data[round_names[i]]
+    except KeyError as e:
+        logger.error(f"KeyError during card conversion: {e}")
+        raise
+
+    player1_strength = evaluator.evaluate(board, player1_cards)
+    player2_strength = evaluator.evaluate(board, player2_cards)
+
+    player1_hand_type = get_hand_type(evaluator.get_rank_class(player1_strength))
+    player2_hand_type = get_hand_type(evaluator.get_rank_class(player2_strength))
+
+    return player1_hand_type, player2_hand_type, player1_strength, player2_strength
+
+
+def proceed_to_next_round():
+    try:
+        matches_ref = db.collection('matches')
+        matches = matches_ref.stream()
+
+        for match in matches:
+            print(match.id)
+            match_data = match.to_dict()
+            player1_action = match_data['player1']['has_action']
+            player2_action = match_data['player2']['has_action']
+
+            if not player1_action and not player2_action:
+                update_data = {}
+
+                if match_data['round'] == 3:
+                    if match_data['player1']['fold']:
+                        update_data['winner'] = 'player2'
+                    elif match_data['player2']['fold']:
+                        update_data['winner'] = 'player1'
+                    elif player1_strength < player2_strength:
+                        update_data['winner'] = 'player1'
+                    elif player2_strength < player1_strength:
+                        update_data['winner'] = 'player2'
+                    else:
+                        update_data['winner'] = 'draw'
+
+                if match_data['round'] == 0:
+                    update_data['round'] = 1
+                    logger.info(
+                        f"Now in Flop {match.id}: Flop is {[Card.int_to_pretty_str(match_data['flop'][i]) for i in range(3)]}")
+                elif match_data['round'] == 1:
+                    update_data['round'] = 2
+                    logger.info(f"Now in Turn {match.id}: Turn is {Card.int_to_pretty_str(match_data['turn'][0])}")
+                elif match_data['round'] == 2:
+                    update_data['round'] = 3
+                    logger.info(f"Now in River {match.id}: River is {Card.int_to_pretty_str(match_data['river'][0])}")
+                elif match_data['round'] == 3:
+                    logger.info(f"Game has ended! Winner is {match_data['winner']}")
+
+                # Evaluate hands
+                player1_hand_type, player2_hand_type, player1_strength, player2_strength = evaluate_hands(match_data,
+                                                                                                          match.id)
+
+                update_data['player1.hand_type'] = player1_hand_type
+                update_data['player2.hand_type'] = player2_hand_type
+
+                # Determine who initially had the action and give it back to them
+                if match_data['player1']['fold'] or match_data['player2']['fold']:
+                    update_data['player1.has_action'] = False
+                    update_data['player2.has_action'] = False
+                else:
+                    initial_action = match_data['initial_action']
+                    update_data['player1.has_action'] = initial_action == 'player1'
+                    update_data['player2.has_action'] = initial_action == 'player2'
+
+                # Update the match document
+                matches_ref.document(match.id).update(update_data)
+    except Exception as e:
+        logger.error(f"Error in proceed_to_next_round: {e}", exc_info=True)
+
+def monitor_match():
+    logger.info("Monitoring matches...")
+    try:
+        while True:
+            proceed_to_next_round()
+            time.sleep(5)  # Check every 5 seconds
+    except Exception as e:
+        logger.error(f"Error in monitor_match: {e}", exc_info=True)
+
+
+if __name__ == "__main__":
+    monitor_match()
